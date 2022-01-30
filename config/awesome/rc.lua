8db46012c41a573fcf33a7e7c508dfcfb659164f
Johannes Müller <muellerjohannes23@gmail.com>
2022-01-30 17:57:30 +0100

    Fix awesome config

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -50,7 +50,7 @@ end
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = os.getenv("TERMINAL") "alacritty"
+terminal = os.getenv("TERMINAL") or "alacritty"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
