9232ea2a813744217845db9884bc1f5268cb8a67
Johannes Müller <johannes.mueller@freiheit.com>
2022-10-18 16:47:56 +0200

    eclipse jdt ls attempts

--- users/modules/nvim/default.nix
+++ users/modules/nvim/default.nix
@@ -1,6 +1,46 @@
 { pkgs, inputs, ... }:
 
 let
+  ftJava = ''
+    -- If you started neovim within `~/dev/xy/project-1` this would resolve to `project-1`
+    local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+    local workspace_dir = '/home/jo/.jdt-workspace/' .. project_name
+
+    local config = {
+      cmd = {
+        '${pkgs.jdt-language-server}/bin/java-language-server',
+        '-configuration', '~/.cache/jdtls',
+        '-data', workspace_dir 
+      },
+
+      -- 💀
+      -- This is the default if not provided, you can remove it. Or adjust as needed.
+      -- One dedicated LSP server & client will be started per unique root_dir
+      root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
+
+      -- Here you can configure eclipse.jdt.ls specific settings
+      -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+      -- for a list of options
+      settings = {
+        java = {
+        }
+      },
+
+      -- Language server `initializationOptions`
+      -- You need to extend the `bundles` with paths to jar files
+      -- if you want to use additional eclipse.jdt.ls plugins.
+      --
+      -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+      --
+      -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+      init_options = {
+        bundles = {}
+      },
+    }
+    -- This starts a new client & server,
+    -- or attaches to an existing client & server depending on the `root_dir`.
+    -- require('jdtls').start_or_attach(config)
+  '';
   nvim-surround = pkgs.vimUtils.buildVimPlugin {
     name = "nvim-surround";
     src = inputs.nvim-surround-src;
@@ -57,7 +97,7 @@ in
       # text objects
       nvim-surround
       nvim-autopairs
-      targets-vim  
+      targets-vim
 
       nvim-which-key
 
@@ -228,5 +268,6 @@ in
       source = ./lua;
       recursive = true;
     };
+    "nvim/ftplugin/java.lua".text = ftJava;
   };
 }
