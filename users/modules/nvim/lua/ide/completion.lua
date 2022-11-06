3abd3812b891bc586e14b4d0dd4d702ff1cbec6b
Johannes Müller <muellerjohannes23@gmail.com>
2022-11-06 23:09:02 +0100

    luasnip config

--- users/modules/nvim/lua/ide/completion.lua
+++ users/modules/nvim/lua/ide/completion.lua
@@ -2,17 +2,17 @@ local cmp = require("cmp")
 
 local luasnip = require("luasnip")
 require("luasnip.loaders.from_vscode").lazy_load()
+luasnip.setup({
+    -- https://www.reddit.com/r/neovim/comments/yiimig/cmp_luasnip_jump_points_strange_behaviour/
+    region_check_events = 'InsertEnter',
+    delete_check_events = 'InsertLeave',
+})
 
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 
-local check_backspace = function()
-    local col = vim.fn.col(".") - 1
-    return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-end
-
 local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 local kind_icons = {
