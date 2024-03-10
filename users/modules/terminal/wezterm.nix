a7e24ff05ea68391e9e475730f1c46668843f432
Johannes Müller <muellerjohannes23@gmail.com>
2024-03-10 21:07:47 +0100

    upgrade everything

--- users/modules/terminal/wezterm.nix
+++ users/modules/terminal/wezterm.nix
@@ -8,7 +8,7 @@
         font = wezterm.font('JetBrainsMonoNL Nerd Font Mono', { weight = 'Bold'}),
         font_size = ${if pkgs.stdenv.isDarwin then "16" else "12"},
         hide_tab_bar_if_only_one_tab = true,
-        use_fancy_tab_bar = true,
+        use_fancy_tab_bar = false,
         enable_scroll_bar = true,
         max_fps = 120,
         window_decorations = 'INTEGRATED_BUTTONS|RESIZE',
