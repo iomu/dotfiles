b6c152d76da0be256f62a6d1019623635a381009
Johannes Müller <johannes.mueller@freiheit.com>
2025-12-01 11:01:37 +0100

    csharp language server

--- users/hosts/mac.nix
+++ users/hosts/mac.nix
@@ -7,5 +7,5 @@
     ghostty = pkgs.ghostty-bin;
   };
 
-  home.packages = [ pkgs.graphviz ];
+  home.packages = [ pkgs.graphviz inputs.csharp-language-server.packages."${builtins.currentSystem}".csharp-language-server ];
 }
